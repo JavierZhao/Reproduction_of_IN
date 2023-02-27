9c76d0e2179bc82ff408df6e8b225772a2aec4bb
JavierZhao <zhaoziha@usc.edu>
2023-02-27 13:34:31 -0800

    created repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We reproduce the results in the paper https://arxiv.org/abs/1909.12285, Interaction networks for the identification of boosted $H\to b\overline{b}$ decays.',
+    author='Arturo Sorensen, Danylo Drohobytsky, Fredy Ramirez, and Zihan Zhao',
+    license='MIT',
+)
