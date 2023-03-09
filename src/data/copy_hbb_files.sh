f2fa2774be30ed0fa26a8f1028eef10234e0f5dc
Zihan Zhao <zhaoziha@usc.edu>
2023-03-09 15:39:16 -0800

    Update copy_hbb_files.sh

--- src/data/copy_hbb_files.sh
+++ src/data/copy_hbb_files.sh
@@ -1,5 +1,6 @@
 # Author: Javier Duarte
 # https://gist.github.com/jmduarte/7adad868dd59ede56dfa8b203bad592e
+
 # testing files
 wget https://opendata.cern.ch/record/12102/files/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC_test_root_file_index.txt
 mkdir -p HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test
